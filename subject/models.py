6a88ec732635401470467f883015defc212544ed
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-08 16:09:02 +0300

    create models: Subject, Score

---
+++ subject/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=30)
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Score(models.Model):
+    subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
+    pupil = models.CharField(max_length=20,default='Ученик')
+    score = models.PositiveIntegerField()
+    date_created = models.DateTimeField(default=None)
+
+
