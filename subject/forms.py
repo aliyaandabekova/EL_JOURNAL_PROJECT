e6f9fea12e5e623419af1c24781fd6ca1a67bf68
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-08 18:40:05 +0300

    create form for score

--- subject/forms.py
+++ subject/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import *
 
 class ScoreForm(forms.ModelForm):
+    score = forms.IntegerField(min_value=2,max_value=5)
     class Meta:
         model = Score
-        fields = ['subject','pupil', 'score']
+        fields = ['subject','pupil', 'score',]
