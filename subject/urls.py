01ce41a1b35ec87498c7cc8f13ae0723f0c50dd2
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-08 18:44:57 +0300

    add path for: homepage, login

--- subject/urls.py
+++ subject/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('',homepage,name='home'),
     path('subjects/',subject_page, name='subjects'),
     path('scores/',score_page, name='scores'),
+    path('login/',login_page,name='login')
 
 
 ]
