b41cdc827f9ddef4b91a8bb3375d01a8f0fc4538
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-10 18:02:30 +0300

    update function for homepage, score page

--- subject/views.py
+++ subject/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render,redirect
 from .models import *
-from .forms import *
+from .forms import ScoreForm
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
-# Create your views here.
+from pupil.services import avg_score_count
+from django.contrib.auth.models import AnonymousUser
 
 def homepage(request):
-    return HttpResponse('Welcome!')
+    return render(request, 'homepage.html')
 
 def subject_page(request): # предметы авторизованного учителя
-    subjects = Subject.objects.filter(teacher=request.user)
+    if isinstance(request.user,AnonymousUser):
+        return HttpResponse('Вы не авторизованы!')
+    else:
+        subjects = Subject.objects.filter(teacher=request.user)
         # teacher = User.objects.get(id=teacher_id)
         # subjects = teacher.subject_set.all()
-    return render(request, 'subject_page.html', {'subjects':subjects})
+        return render(request, 'subject_page.html', {'subjects':subjects})
 
 
 def login_page(request):
@@ -24,13 +28,25 @@ def login_page(request):
         return redirect('subjects')
     return render(request, 'login.html')
 
-def score_page(request):
+def score_page(request,pupil_id,subject_id):
+    subject = Subject.objects.get(id=subject_id)
+    pupil = Pupil.objects.get(id=pupil_id)
+    scores = pupil.score_set.all()
+    print(scores)
     form = ScoreForm()
-    subjects = Subject.objects.filter(teacher=request.user)
     if request.method == 'POST':
         form = ScoreForm(request.POST)
         if form.is_valid():
-            form.save()
-    return render(request, 'score_page.html', {'form':form})
+            score = form.cleaned_data['score']
+            Score.objects.create(pupil=pupil,score=score,subject=subject)
+            avg_score_count(scores,pupil)
+    return render(request,'score_page.html',{'pupil':pupil,'form':form})
+
+
+
+
+
+
+
 
 
