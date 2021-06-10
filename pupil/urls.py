9df11958c1ca36312b8da5022c75d78fe89d8f6e
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-10 17:49:04 +0300

    add urls for pupils_detail and pupil_detail

--- pupil/urls.py
+++ pupil/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
 
-url_patterns = [
-    path('pupils/',pupil_page, name='pupils'),
+urlpatterns = [
+    path('pupils/<int:subject_id>/',pupil_page,name='pupils'),
+    path('pupils_detail/',pupils_detail,name='pupils_detail'),
+    path('pupil_detail/<int:pupil_id>/',pupil_detail,name='pupil_detail'),
 
 ]
