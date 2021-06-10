a0ea84dafc7329f2038ba4d599917d0f6b788e6f
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-10 17:50:20 +0300

    create functions for: pupils_detail, pupil_detail

--- pupil/views.py
+++ pupil/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from .models import *
 from subject.models import *
-# Create your views here.
 
-def pupil_page(request):
-    if request.method == 'GET':
-        subjects = Subject.objects.filter()
+
+def pupil_page(request,subject_id):
+    subjects = Subject.objects.get(id=subject_id)
+    pupils = Pupil.objects.filter(subject=subjects)
+    return render(request,'pupil_page.html',{'pupils':pupils, 'subject':subjects})
+
+def pupils_detail(request):
+    subjects = Subject.objects.filter(teacher=request.user)
+    pupils = Pupil.objects.filter(subject__in=subjects).distinct()
+    return render(request, 'pupils_detail.html', {'pupils':pupils})
+
+def pupil_detail(request,pupil_id):
+    subjects_of_teacher = Subject.objects.filter(teacher=request.user)
+    pupil = Pupil.objects.get(id=pupil_id)
+    subjects_of_pupil = pupil.subject.filter(teacher=request.user)
+    scores = Score.objects.filter(pupil=pupil,subject__in=subjects_of_pupil)
+    return render(request,'pupil_detail.html', {'pupil':pupil,'subjects_of_pupil':subjects_of_pupil,'scores':scores})
+
