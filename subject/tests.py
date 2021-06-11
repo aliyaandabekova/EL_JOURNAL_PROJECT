752e88febe1401539ccd266830bb0946a9973cc9
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-11 17:28:41 +0300

    tests for: homepage, my subjects, score

--- subject/tests.py
+++ subject/tests.py
@@ -2,25 +2,42 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import *
 
-# Create your tests here.
 
-class SubjectTest(TestCase):
-    def setUp(self):
-        self.url = reverse('subjects')
-        self.user = User.objects.create_user(username='aliya',password='aliya.123456')
-        Subject.objects.create(title='Математика', teacher=self.user)
-    def test_subject_get(self):
-        self.response = self.client.get(self.url)
+class HomepageTest(TestCase):
+    def setUp(self) -> None:
+        self.url = reverse('home')
+        self.user = User.objects.create_user(username='aliya',password='123456')
+    def test_homepage(self):
+        self.client.login(username='aliya',password='123456')
+        self.response = self.client.post(self.url)
         self.assertEqual(self.response.status_code,200)
 
-class ScoreTest(TestCase):
-    def setUp(self):
-        self.url = reverse('scores')
-        self.user = User.objects.create_user(username='aliya',password='aliya.123456')
-        self.subject = Subject.objects.create(title='Биология', teacher=self.user)
-        Score.objects.create(subject=self.subject,pupil='Ученик',score=5)
-    def test_score_get(self):
+
+
+class SubjectpageTest(TestCase):
+    def setUp(self) -> None:
+        self.url = reverse('subjects')
+        self.teacher = User.objects.create(username='aliya',password='aliya.123456')
+        self.subjects = Subject.objects.create(title='Math',teacher=self.teacher)
+    def test_subject_page(self):
         self.client.login(username='aliya',password='aliya.123456')
         self.response = self.client.get(self.url)
         self.assertEqual(self.response.status_code,200)
 
+
+
+class ScoreTest(TestCase):
+    def setUp(self) -> None:
+        self.teacher = User.objects.create_user(username='aliya',password='aliya.1234')
+        self.subject = Subject.objects.create(title='Math',teacher=self.teacher)
+        self.pupil = Pupil.objects.create(name='pupil1',avg_score=3.5)
+        self.pupil.subject.add(self.subject)
+        self.scores = Score.objects.create(subject=self.subject,pupil=self.pupil,score=5)
+        self.url = reverse('scores', args=(self.pupil.id, self.subject.id,))
+    def test_scorepage(self):
+        data = {
+            'score':4,
+        }
+        self.client.login(username='aliya',password='aliya.1234')
+        self.response = self.client.post(self.url,data)
+        self.assertEqual(self.response.status_code,200)
