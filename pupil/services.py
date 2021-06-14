554d4b75d71f3821ea67e3fabc76d6e0b8abca08
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-14 13:50:12 +0300

    add round(avg_score,1)

--- pupil/services.py
+++ pupil/services.py
@@ -6,5 +6,6 @@ def avg_score_count(scores,pupil):
     for score_object in scores:
         summ += score_object.score
     avg_score = summ / len(scores)
+    avg_score = round(avg_score,1)
     pupil.avg_score = avg_score
     pupil.save()
